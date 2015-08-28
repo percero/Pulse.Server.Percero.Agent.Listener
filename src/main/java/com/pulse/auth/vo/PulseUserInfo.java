3e0f0b8e43857473fc8a62d9311d9c1c737911dc
Collin Brown <curb47@gmail.com>
2015-08-28 13:53:08 -0700

    Adding PulseHttpAuthProvider

---
+++ src/main/java/com/pulse/auth/vo/PulseUserInfo.java
@@ -0,0 +1,29 @@
+package com.pulse.auth.vo;
+
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+
+/**
+ * Class to represent the response from the Pulse /retrieve_user endpoint
+ * Created by Jonathan Samples on 8/27/15.
+ */
+@JsonIgnoreProperties(ignoreUnknown=true)
+public class PulseUserInfo {
+    private String userLogin;
+    private String employeeId;
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getUserLogin() {
+        return userLogin;
+    }
+
+    public void setUserLogin(String userLogin) {
+        this.userLogin = userLogin;
+    }
+}
