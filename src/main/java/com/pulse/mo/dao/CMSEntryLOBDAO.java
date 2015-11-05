dee85c9ac20fa7a229750bc4613fb6a3727dea32
Manoj P M <manoj.pm@v2solutions.com>
2015-11-05 13:41:17 +0530

    reverted to the handmade changes on CMSEntryLOBDAO

--- src/main/java/com/pulse/mo/dao/CMSEntryLOBDAO.java
+++ src/main/java/com/pulse/mo/dao/CMSEntryLOBDAO.java
@@ -41,7 +41,7 @@ public class CMSEntryLOBDAO extends SqlDataAccessObject<CMSEntryLOB> implements 
 	public static final String CONNECTION_FACTORY_NAME = "cms";
 
 	//TODO:For use refactoring, so we set it once
-	public static final String SQL_VIEW = "SELECT  '' as \"ID\", AGENT_LOB_SITE.CLIENT_DESCR as \"LOB_NAME\", AGENT_LOB_SITE.CLIENT_DESCR as \"CLIENT_NAME\", AGENT_LOB_SITE.CLIENT_ID as \"CLIENT_ID\", CMS_ENTRY.EMPLOYEEID as \"EMPLOYEE_ID\", AGENT_LOB_SITE.SITE_DESCR as \"SITE_NAME\", CLIENT_SITE.SITE_ID as \"SITE_ID\", AGENT_LOB_SITE.ECP_LOB_ID as \"LOB_ID\", CMS_ENTRY.ID as \"CMS_ENTRY_ID\" FROM \"MOB_CMS_DATA_VW\" \"CMS_ENTRY_LOB\" ";
+	public static final String SQL_VIEW = "SELECT  AGENT_LOB.ECP_LOB_ID as \"ID\", CLIENT_SITE.SITE_ID as \"SITE_ID\", CMS_ENTRY.EMPLOYEEID as \"EMPLOYEE_ID\", AGENT_LOB_SITE.SITE_DESCR as \"SITE_NAME\", AGENT_LOB_SITE.CLIENT_DESCR as \"LOB_NAME\", AGENT_LOB_SITE.CLIENT_DESCR as \"CLIENT_NAME\", AGENT_LOB_SITE.CLIENT_ID as \"CLIENT_ID\", CMS_ENTRY.ID as \"CMS_ENTRY_ID\", AGENT_LOB_SITE.ECP_LOB_ID as \"LOB_ID\" FROM \"MOB_CMS_DATA_VW\" \"CMS_ENTRY_LOB\" ";
 	private String selectFromStatementTableName = " FROM \"PULSE\".\"MOB_CMS_DATA_VW\" \"CMS_ENTRY_LOB\"";
 	private String whereClause = "Join PULSE.MOB_EMP_LOB_VW AGENT_LOB On AGENT_LOB.EMPLOYEE_ID = CMS_ENTRY.EMPLOYEE_ID Join PULSE.MOB_CLIENT_SITE_VW CLIENT_SITE On CLIENT_SITE.ID =AGENT_LOB.CLIENT_ID Join PULSE.MOB_LOB_SITE_CLIENT_VW AGENT_LOB_SITE On AGENT_LOB_SITE.CLIENT_ID =AGENT_LOB.CLIENT_ID And AGENT_LOB_SITE.SITE_ID =CLIENT_SITE.SITE_ID WHERE AGENT_LOB.ECP_LOB_ID=?";
 	private String whereInClause = "Join PULSE.MOB_EMP_LOB_VW AGENT_LOB On AGENT_LOB.EMPLOYEE_ID = CMS_ENTRY.EMPLOYEE_ID Join PULSE.MOB_CLIENT_SITE_VW CLIENT_SITE On CLIENT_SITE.ID =AGENT_LOB.CLIENT_ID Join PULSE.MOB_LOB_SITE_CLIENT_VW AGENT_LOB_SITE On AGENT_LOB_SITE.CLIENT_ID =AGENT_LOB.CLIENT_ID And AGENT_LOB_SITE.SITE_ID =CLIENT_SITE.SITE_ID Join Table(sys.dbms_debug_vc2coll(?)) SQLLIST On AGENT_LOB.ECP_LOB_ID= SQLLIST.column_value";
@@ -57,7 +57,7 @@ public class CMSEntryLOBDAO extends SqlDataAccessObject<CMSEntryLOB> implements 
 
 	@Override
 	protected String getSelectShellOnlySQL() {
-		return "SELECT '' as \"ID\" " + selectFromStatementTableName + whereClause;
+		return "SELECT AGENT_LOB.ECP_LOB_ID as \"ID\" " + selectFromStatementTableName + whereClause;
 	}
 
 	@Override
@@ -67,12 +67,12 @@ public class CMSEntryLOBDAO extends SqlDataAccessObject<CMSEntryLOB> implements 
 
 	@Override
 	protected String getSelectAllShellOnlySQL() {
-		return "SELECT '' as \"ID\" " + selectFromStatementTableName +  orderByTableName;
+		return "SELECT AGENT_LOB.ECP_LOB_ID as \"ID\" " + selectFromStatementTableName +  orderByTableName;
 	}
 
 	@Override
 	protected String getSelectAllShellOnlyWithLimitAndOffsetSQL() {
-		return "SELECT '' as \"ID\" " + selectFromStatementTableName  +  orderByTableName  + " LIMIT ? OFFSET ?";
+		return "SELECT AGENT_LOB.ECP_LOB_ID as \"ID\" " + selectFromStatementTableName  +  orderByTableName  + " LIMIT ? OFFSET ?";
 	}
 
 	@Override
@@ -98,7 +98,7 @@ public class CMSEntryLOBDAO extends SqlDataAccessObject<CMSEntryLOB> implements 
 	@Override
 	protected String getSelectInShellOnlySQL() 
 	{
-		return "SELECT '' as \"ID\" " + selectFromStatementTableName +  whereInClause;
+		return "SELECT AGENT_LOB.ECP_LOB_ID as \"ID\" " + selectFromStatementTableName +  whereInClause;
 	}
 
 	@Override
@@ -112,15 +112,13 @@ public class CMSEntryLOBDAO extends SqlDataAccessObject<CMSEntryLOB> implements 
 	protected String getSelectByRelationshipShellOnlySQL(String joinColumnName) 
 	{
 
-		if (joinColumnName.equalsIgnoreCase("\"CMS_ENTRY_ID\"")) {
-			return "SELECT '' as \"ID\" " + selectFromStatementTableName + " WHERE \"CMS_ENTRY_LOB\"." + joinColumnName + "=?";
-		}
-		return "SELECT '' as \"ID\" " + selectFromStatementTableName + " WHERE \"CMS_ENTRY_LOB\"." + joinColumnName + "=?";
+
+		return "SELECT AGENT_LOB.ECP_LOB_ID as \"ID\" " + selectFromStatementTableName + " Join PULSE.MOB_EMP_LOB_VW AGENT_LOB On AGENT_LOB.EMPLOYEE_ID = CMS_ENTRY.EMPLOYEE_ID Join PULSE.MOB_CLIENT_SITE_VW CLIENT_SITE On CLIENT_SITE.ID =AGENT_LOB.CLIENT_ID Join PULSE.MOB_LOB_SITE_CLIENT_VW AGENT_LOB_SITE On AGENT_LOB_SITE.CLIENT_ID =AGENT_LOB.CLIENT_ID And AGENT_LOB_SITE.SITE_ID =CLIENT_SITE.SITE_ID WHERE \"CMS_ENTRY_LOB\"." + joinColumnName + "=?";
 	}
 
 	@Override
 	protected String getFindByExampleSelectShellOnlySQL() {
-		return "SELECT '' as \"ID\" " + selectFromStatementTableName;
+		return "SELECT AGENT_LOB.ECP_LOB_ID as \"ID\" " + selectFromStatementTableName;
 	}
 
 	@Override
