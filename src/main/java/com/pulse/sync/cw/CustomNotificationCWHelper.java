cb8abb1a4e8c26acabeb4cf6815725ff073586ef
Nikish Parikh <nikish.parikh@v2solutions.com>
2016-01-14 20:58:44 +0530

    estart notif uncommented

--- src/main/java/com/pulse/sync/cw/CustomNotificationCWHelper.java
+++ src/main/java/com/pulse/sync/cw/CustomNotificationCWHelper.java
@@ -407,10 +407,10 @@ public class CustomNotificationCWHelper extends ChangeWatcherHelper {
 
 
                                             //Generate Invalid Activity Code Notification if TimecardEntry Activity's activity code is not in the list of valid code
-//                                            generateInvalidActivityCodeNotification(timecardEntry, agent, teamLeader, validActivityCodeList, lobConfiguration);
-//
-//                                            //Generate NonBillable Activity Code Notification if TimecardEntry Activity's activity code is in the list of NonBillable code
-//                                            generateNonBillableActivityCodeNotification(timecardEntry, agent, teamLeader, nonBillableActivityCodeList, lobConfiguration);
+                                            generateInvalidActivityCodeNotification(timecardEntry, agent, teamLeader, validActivityCodeList, lobConfiguration);
+
+                                            //Generate NonBillable Activity Code Notification if TimecardEntry Activity's activity code is in the list of NonBillable code
+                                            generateNonBillableActivityCodeNotification(timecardEntry, agent, teamLeader, nonBillableActivityCodeList, lobConfiguration);
 
 
                                         }
